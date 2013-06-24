0b42a14dec98659a73aa7d0dc3d61c0d2eec60d1
Zach Latta <zchlatta@gmail.com>
2013-06-23 21:17:30 -0700

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Easymeme::Application.config.secret_key_base = 'e08f991e3423bd91aef492589ca9e108c59c1c602ebd3d6e2f71daaa251a40f07c3af45d4d4530e63396739852802b72755952161efda93f3ef23734950f047a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Easymeme::Application.config.session_store :cookie_store, key: '_easymeme_session'
