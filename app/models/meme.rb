29b174a167818eef2d4aa32154e718970d7437e5
Zach Latta <zchlatta@gmail.com>
2013-06-24 22:16:11 -0700

    Add contents of meme model.

--- app/models/meme.rb
+++ app/models/meme.rb
@@ -1,2 +1,12 @@
 class Meme < ActiveRecord::Base
+
+  before_save {
+    self.title = title.downcase
+    self.top_text = top_text.downcase
+    self.bottom_text = bottom_text.downcase
+  }
+  
+  validates_presence_of :title, :url
+  validates :top_text,    presence: { if: bottom_text.blank? }
+  validates :bottom_text, presence: { if: top_text.blank? }
 end
