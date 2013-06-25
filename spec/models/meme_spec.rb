13077fdff8bd3dcad0bfed0a84300b1f454e0940
Zach Latta <zchlatta@gmail.com>
2013-06-24 22:04:20 -0700

    Add meme spec.

--- spec/models/meme_spec.rb
+++ spec/models/meme_spec.rb
@@ -1,5 +1,33 @@
 require 'spec_helper'
 
 describe Meme do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before { @meme = Meme.new(title: "bad luck brian",
+                            top_text: "has a pet rock",
+                            bottom_text: "it runs away",
+                            url: "http://i.imgur.com/xpeoxxh.jpg") }
+
+  subject { @meme }
+
+  it { should respond_to(:title) }
+  it { should respond_to(:top_text) }
+  it { should respond_to(:bottom_text) }
+  it { should respond_to(:url) }
+
+  it { should be_valid }
+
+  describe "when title is not present" do
+
+    before { @meme.title = " " }
+    it { should_not be_valid }
+  end
+
+  describe "when top and bottom text are not present" do
+
+    before {
+      @meme.top_text = " "
+      @meme.bottom_text = " "
+    }
+    it { should_not be_valid }
+  end
 end
