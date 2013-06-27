2065a36577315dbe73950b3441db4428eaeddf17
Zach Latta <zchlatta@gmail.com>
2013-06-27 03:13:55 -0700

    Refactor static pages test.

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -4,20 +4,25 @@ describe "static pages" do
 
   subject { page }
 
+  shared_examples_for "all static pages" do
+    it { should have_content(heading) }
+    it { should have_title(full_title(page_title)) }
+  end
+
   describe "home page" do
 
     before { visit root_path }
 
-    it { should have_selector('a', text: 'Easymeme') }
-    it { should have_title(full_title('')) }
+    let(:heading)    { 'Easymeme' }
+    let(:page_title) { '' }
   end
 
   describe "about page" do
     
     before { visit about_path }
 
-    it { should have_selector('h1', text: 'About') }
-    it { should have_title(full_title('About')) }
+    let (:heading)   { 'About' }
+    let(:page_title) { full_title('About') }
   end
 
   it "should have the right links on the layout" do
